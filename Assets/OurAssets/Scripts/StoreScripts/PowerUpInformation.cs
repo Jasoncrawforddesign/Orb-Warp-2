e95ccf2a54ca7eed04750b8aa65997b16e343e17
Jason_Home <jasoncrawford.design@gmail.com>
2020-05-07 16:28:34 +1000

    Added Scriptable Object for Store powerups
    Added a script to read the scriptable objects, and also added scriptable/scrolling view on the store.

---
+++ Assets/OurAssets/Scripts/StoreScripts/PowerUpInformation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PowerUp", menuName = "Powerup")]
+public class PowerUpInformation : ScriptableObject
+{
+	public string powerUpName;
+	public string description;
+
+	public Sprite artwork;
+
+	public int upgradeCost;
+
+	public int currentLevel;
+
+
+}
---
+++ Assets/OurAssets/Scripts/StoreScripts/Upgrade_Info.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class Upgrade_Info : MonoBehaviour
+{
+	public PowerUpInformation info;
+
+	public TextMeshProUGUI powerUpNameSlot;
+	public TextMeshProUGUI itemLevel;
+	public TextMeshProUGUI itemDescription;
+	public Image imageArtwork;
+    // Start is called before the first frame update
+    void Start()
+    {
+		powerUpNameSlot.text = info.powerUpName;
+		itemLevel.text = info.currentLevel.ToString();
+		itemDescription.text = info.description;
+
+		imageArtwork.sprite = info.artwork;
+
+	}
+
+
+}
